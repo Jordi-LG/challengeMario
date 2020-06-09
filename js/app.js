3b8059898162bfb2b6edde38be114d1e7f074634
Jordi-LG <jordy.leguet@gmail.com>
2020-06-09 13:04:14 +0200

    button to change boolean star

--- js/app.js
+++ js/app.js
@@ -1,10 +1,38 @@
+let btnStar = {
+  props: ['star-message-btn'],
+
+  template: `<button @click="status"> {{ starMessageBtn }} </button>`,
+
+  methods: {
+    status() {
+      this.$emit('status')
+    }
+  }
+}
+
 let vm = new Vue({
   el: "#app",
+  components: { btnStar },
+  
   data: {
+    title: 'Salut',
     message: "Mon message",
     star: false,
+    starMessageBtn: 'Collect a star',
     mushroom: false,
     lives: 3,
+  },
+
+  methods: {
+    changeStar: function() {      
+      if(this.star === false) {
+        this.star = true;
+        this.starMessageBtn = 'Remove star';
+      } else {
+        this.star = false;
+        this.starMessageBtn = 'Collect a star';
+      }
+    }
   }
 
 })
